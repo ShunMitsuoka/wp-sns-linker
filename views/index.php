779deee9dc8f7bcd5bd15a1fefdbe017f66ff482
s.mitsuoka <s.mitsuoka@and-ex.co.jp>
2023-05-22 13:35:08 +0900

    デザイン修正

--- views/index.php
+++ views/index.php
@@ -4,22 +4,35 @@ $args = array(
 	'numberposts'	=> 20,
 );
 $my_posts = get_posts( $args );
-
-foreach ( $my_posts as $post ): // $datas as $post の $datas は取得時に設定した変数名、$postは変更不可
 ?>
-<div>
-    <form action="" method="GET">
-        <div>
-            <?=$post->post_title?>
-        </div>
-        <input type="hidden" name="page" value="snslinker">
-        <input type="hidden" name="sl-page" value="post">
-        <input type="hidden" name="post-id" value="<?=$post->ID?>">
-        <button type="submit">sns連携</button>
-    </form>
-</div>
 
+<table>
+    <thead>
+        <tr>
+            <td>Title</td>
+            <td></td>
+        </tr>
+    </thead>
+    <tbody>
+    <?php
+        foreach ( $my_posts as $post ):
+    ?>
+        <tr>
+            <td><?=$post->post_title?></td>
+            <td>
+                <form action="" method="GET">
+                    <input type="hidden" name="page" value="snslinker">
+                    <input type="hidden" name="sl-page" value="post">
+                    <input type="hidden" name="post-id" value="<?=$post->ID?>">
+                    <button type="submit">sns連携</button>
+                </form>
+            </td>
+        </tr>
+    <?php
+        endforeach; 
+    ?>
+    </tbody>
+</table>
 <?php
-endforeach; 
 wp_reset_postdata();
 ?>
